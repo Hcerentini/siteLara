7db29f1cc38605386910a7416171ba16d8eb3fd2
Robson Steindorff Ceolin <robsonceolinff@gmail.com>
2023-09-26 17:17:07 -0300

    Modal media query style changes: added transform for mobile view

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 document.addEventListener("click",function(e){
-        if(e.target.classList.contains("gallery-item")){
-            const src = e.target.getAttribute("src");
-            document.querySelector(".modal-img").src = src;
-            const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));   
-            myModal.show();
-        }
+
+  const mediaQuery = window.matchMedia('(max-width: 768px)')
+
+  if(e.target.classList.contains("gallery-item")){
+      const src = e.target.getAttribute("src");
+      document.querySelector(".modal-img").src = src;
+      const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));   
+
+      if (mediaQuery.matches) {
+        // myModal._element.transform = 'rotate(-90deg)';
+      }
+
+      myModal.show();
+  }
+
 })
